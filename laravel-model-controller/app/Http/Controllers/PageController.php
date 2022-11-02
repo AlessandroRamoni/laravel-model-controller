13e0a680578d8bcedea2f0e1c82701925b5c2436
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-02 19:49:33 +0100

    edit home

--- laravel-model-controller/app/Http/Controllers/PageController.php
+++ laravel-model-controller/app/Http/Controllers/PageController.php
@@ -3,16 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Models\Movie;
-
+use App\models\Movie;
 class PageController extends Controller
 {
-    public function index(/*Request $request*/)
-    {
-        //$input = $request->all();
-        //var_dump($input);
+    public function index(){
         $movies = Movie::all();
-        return view('home', compact('movies'));
+
+        return view('welcome', compact('movies'));
     }
 }
--- laravel-model-controller/resources/views/home.blade.php
+++ laravel-model-controller/resources/views/home.blade.php
@@ -1,21 +1,17 @@
-@extends('layout/layout_home')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('main')
-    <h1>My DB Movies</h1>
-    <div class="card-container">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
 
-        @foreach ($movies as $movie)
-            <div class="card">
-                <h2>{{ $movie['title'] }}</h2>
-                <h5>Titolo Originale:</h5>
-                <h3>{{ $movie['original_title'] }}</h3>
-                <h5>Nazionalita':</h5>
-                <p>{{ $movie['nationality'] }}</p>
-                <h5>Data Uscita:</h5>
-                <p>{{ $movie['date'] }}</p>
-                <h5>Voto:</h5>
-                <span>{{ $movie['vote'] }}</span>
-            </div>
-        @endforeach
-    </div>
-@endsection
+<body>
+    @foreach ($movies as $item)
+        <h1>{{ $item['title'] }}</h1>
+    @endforeach
+</body>
+
+</html>
